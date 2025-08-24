ed0716830423d260510cfb2bfcf96e0805456de9
InfoMathias <43543036+InfoMathias@users.noreply.github.com>
2025-08-24 08:38:22 -0400

    data types, methods, traits, brief generics overview

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+fn main() {
+    let separated: i32 = 1_000_000;
+
+    let casted = separated as i16;
+
+    let float_value = 3.14159;
+    println!("{:.3}", float_value);
+
+    let with_milk: bool = true;
+    let with_sugar: bool = false;
+
+    let is_my_type_of_coffee = with_milk && with_sugar;
+
+    let is_acceptable_coffee = with_milk || with_sugar;
+
+    let an_array = [1, 2, 3, 4];
+    dbg!(an_array);
+
+    let a_tuple = (separated, float_value, with_milk, an_array);
+    dbg!(a_tuple);
+}
