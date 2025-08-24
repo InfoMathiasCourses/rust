4ae07a416bef456185cf840c9da86ef489d6dc2e
InfoMathias <43543036+InfoMathias@users.noreply.github.com>
2025-08-23 21:41:09 -0400

    variables, mutability, constants, types and compiler instructions

--- variables-and-mutability/src/main.rs
+++ variables-and-mutability/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+const TOUCHDOWN_POINTS: i32 = 6;
+fn main() {
+    let season: &str = "Spring";
+
+    let mut points_scored: i32 = 28;
+    points_scored = 35;
+
+    let event_time = "06:00";
+    let event_time = 6;
+
+    println!("{season} {points_scored} {event_time} {TOUCHDOWN_POINTS}");
+    println!("{} {} {} {}", season, points_scored, event_time, TOUCHDOWN_POINTS);
+    print!("{0} {1} {2} {3}", season, points_scored, event_time, TOUCHDOWN_POINTS);
+
+    #[allow(unused_variables)]
+    let favorite_beverage = "Lipton Ice Tea";
+}
