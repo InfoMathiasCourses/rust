bcbd4deb91b83c12383ee40be5697b07fc74b4c8
InfoMathias <43543036+InfoMathias@users.noreply.github.com>
2025-09-09 18:30:08 -0400

    Control Flow 1 & 2

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,1 +1,37 @@
-fn main() {}
+fn main() {
+    let color = "green";
+    println!("Color {} corresponds to code {}.", color, color_to_number(color));
+
+    let number = 5;
+    println!("No recursion: !{} = {}", number, factorial_no_recursion(number));
+    println!("Recursion: !{} = {}", number, factorial_with_recursion(number));
+}
+
+fn color_to_number(color: &str) -> i32 {
+    match color {
+        "red" => 1,
+        "green" => 2,
+        "blue" => 3,
+        _ => 0
+    }
+}
+
+fn factorial_no_recursion(number: i32) -> i32 {
+    let mut result = 1;
+    let mut count = number;
+
+    while count > 1 {
+        result *= count;
+        count -= 1;
+    }
+
+    result
+}
+
+fn factorial_with_recursion(number: i32) -> i32 {
+    if number < 2 {
+        1
+    } else {
+        number * factorial_with_recursion(number-1)
+    }
+}
--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,1 +1,60 @@
-fn main() {}
+fn main() {
+
+    /* Coding Challenge #1 */
+
+    let is_concert: bool = true;
+    let is_event = is_concert;
+
+    println!("{} {}", is_concert, is_event);
+
+    let sushi: &str = "Salmon";
+    let dinner = sushi;
+
+    println!("{} {}", sushi, dinner);
+
+    let sushi_2: String = String::from("Salmon");
+    let mut dinner_2: String = sushi_2;
+
+    // dinner_2 now owns the value from sushi_2 therefore sushi_2
+    println!("{}", dinner_2);
+
+    eat_meal(&mut dinner_2);
+
+    println!("{dinner_2}");
+
+
+    /* Coding Challenge #2 */
+
+    let mut trip = start_trip();
+    visit_philadelphia(&mut trip);
+    trip.push_str(" and ");
+    visit_new_york(&mut trip);
+    trip.push_str(" and ");
+    visit_boston(&mut trip);
+    trip.push_str(".");
+    show_itinerary(&trip);
+}
+
+fn eat_meal (meal: &mut String) {
+    String::clear(meal);
+}
+
+fn start_trip() -> String {
+    String::from("The plan is...")
+}
+
+fn visit_philadelphia(text: &mut String) {
+    text.push_str("Philadelphia");
+}
+
+fn visit_new_york(text: &mut String) {
+    text.push_str("New York");
+}
+
+fn visit_boston(text: &mut String){
+    text.push_str("Boston");
+}
+
+fn show_itinerary(text: &String){
+    println!("{text}");
+}
